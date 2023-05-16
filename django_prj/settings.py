ce8cad9f4bc97e9a3a4e50fa7d583801ff4b0c82
soobeen <s3238s@naver.com>
2023-05-16 23:33:22 +0900

    Add Bootstrap

--- django_prj/settings.py
+++ django_prj/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gamepage',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+
